9ef5bedc7c4be3e70dd1fd903c105143cab6df6f
nard.calma@gmail.com <nard.calma@gmail.com>
2022-07-05 16:42:42 -0700

    add game class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("test")
+class Game {
+    constructor (name) {
+        this.name = name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        this.age = 0
+    }
+}
+
+const pet = new Game("Tamagochi")
+console.log(pet)
