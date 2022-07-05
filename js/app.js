eb140084080875b8773cc6a542b89109de68a6b8
nard.calma@gmail.com <nard.calma@gmail.com>
2022-07-05 16:57:04 -0700

    add updateStats()

--- js/app.js
+++ js/app.js
@@ -6,7 +6,24 @@ class Game {
         this.boredom = 1
         this.age = 0
     }
+
+    updateStats = () => {
+        let lblName = document.querySelector("#petName")
+        let lblHunger = document.querySelector("#hunger")
+        let lblSleepiness = document.querySelector("#sleepiness")
+        let lblBoredom = document.querySelector("#boredom")
+        let lblAge = document.querySelector("#age")
+        lblName.innerText = this.name
+        lblHunger.innerText = this.hunger
+        lblSleepiness.innerText = this.sleepiness
+        lblBoredom.innerText = this.boredom
+        lblAge.innerText = this.age
+        
+    }
+
 }
 
 const pet = new Game("Tamagochi")
+pet.updateStats()
+
 console.log(pet)
